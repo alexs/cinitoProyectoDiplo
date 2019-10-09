41b2f632c98042888af40f5d53409ae13e79417d
Alex <alexs1010@gmail.com>
2019-10-09 17:03:58 -0500

    Initial Commit

---
+++ cinito/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cinito
+//
+//  Created by Alex on 10/9/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
